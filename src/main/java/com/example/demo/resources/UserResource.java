5ebefb5e2f3b09eee943c73f5418d1a746db5b09
Rodolfo Oliveira <rodolfo.oli29@gmail.com>
2025-11-17 23:09:05 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,21 +1,40 @@
 package com.example.demo.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User  u = new User(1L, "Maria", "maria@.com", "1234565", "33333");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		// User u = new User(1L, "Maria", "maria@.com", "1234565", "33333");
+		List<User> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
 
 	}
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)// @path para declarar e o spring aceitar o
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
 }
